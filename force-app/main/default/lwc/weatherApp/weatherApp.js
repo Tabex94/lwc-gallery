09b97af6105d6b80524a746ef81a96a76b81de28
Tabex94 <josealonso_ca@outlook.es>
2025-05-18 13:03:05 -0600

    Finally parsed data

--- force-app/main/default/lwc/weatherApp/weatherApp.js
+++ force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -2,11 +2,34 @@ import { LightningElement, wire } from 'lwc';
 import getWeatherData from '@salesforce/apex/WeatherAppController.getWeatherData';
 
 export default class weatherApp extends LightningElement {
+
+    formatedData='';
     @wire(getWeatherData) weatherData;
-    weatherData;
+
+    /*@wire(getWeatherData) weatherData({ error, data }){
+        if(data){
+            
+            console.log('Weather data:', data);
+            try {
+                this.formatedData = typeof data === 'string' ? JSON.parse(data) : data;
+                console.log('Formatted Weather data:', this.formatedData);
+                } catch(parseError) {
+                    console.error('Error parsing weather data:', parseError);
+                }
+            
+        } else if(error){
+            console.error('Error fetching weather data:', error);
+        }
+    }*/
+
+    //weatherDataObje = this.weatherData.data;
     
     get temperature() {
-        return this.weatherData.data ? this.weatherData.data.temperature : 'Loading...';
+        return this.weatherData.data;
+    }
+
+    get isDay(){
+        return this.weatherData.data;
     }
 
 }
