337435538a87f5a5dd32d019f53b508e5f09f12e
Tabex94 <josealonso_ca@outlook.es>
2025-05-18 03:54:47 -0600

    Sunny with posibility of great things

--- force-app/main/default/lwc/weatherApp/weatherApp.js
+++ force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -1,3 +1,12 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getWeatherData from '@salesforce/apex/WeatherAppController.getWeatherData';
 
-export default class WeatherApp extends LightningElement {}
+export default class weatherApp extends LightningElement {
+    @wire(getWeatherData) weatherData;
+    weatherData;
+    
+    get temperature() {
+        return this.weatherData.data ? this.weatherData.data.temperature : 'Loading...';
+    }
+
+}
