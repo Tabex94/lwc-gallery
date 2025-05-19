3fb804e244ac59f8086ec6957e37f4ecf5b68227
Tabex94 <josealonso_ca@outlook.es>
2025-05-18 19:49:28 -0600

    Board mounted

--- force-app/main/default/lwc/opportunityBoard/opportunityBoard.js
+++ force-app/main/default/lwc/opportunityBoard/opportunityBoard.js
@@ -4,10 +4,11 @@ export default class OpportunityBoard extends LightningElement {
   @track opportunities = [
     { Id: '001', Name: 'Deal 1', Amount: 5000, StageName: 'Qualification' },
     { Id: '002', Name: 'Deal 2', Amount: 8000, StageName: 'Proposal' },
-    { Id: '003', Name: 'Deal 3', Amount: 12000, StageName: 'Closed Won' }
+    { Id: '003', Name: 'Deal 3', Amount: 12000, StageName: 'Closed Won' },
+    { Id: '004', Name: 'Deal 3', Amount: 12000, StageName: 'Qualification' }
   ];
 
-  stageList = ['Qualification', 'Proposal', 'Closed Won'];
+  stageList = ['Qualification', 'Proposal', 'Closed Won', 'Closed Lost'];
 
   get columns() {
     return this.stageList.map(stage => ({
